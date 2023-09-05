287ac887548c8fcfb1fd6f5cb1a0b81a6e362522
cwojdzinskaaleksandra <cwojdzinska.aleksandra@gmail.com>
2023-09-05 11:19:05 +0200

    page improvement

--- main.js
+++ main.js
@@ -7,14 +7,29 @@ window.addEventListener('scroll', () => {
         const sectionTop = section.offsetTop;
         const sectionHeight = section.clientHeight;
         if (scrollY >= sectionTop - sectionHeight / 3) {
-            current = section.getAttribute('id');           
+            current = section.getAttribute('id');
         }
-   });
+    });
 
-   navLi.forEach( li =>{
-    li.classList.remove('active');
-    if(li.classList.contains(current)){
-        li.classList.add('active')
-    }
-   })
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+            li.classList.add('active')
+        }
+    })
 })
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const link = document.querySelectorAll('nav ul a');
+
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active');
+// }, false);
+
+// for (let i = 0; i < link.length; i++) {
+//     link[i].addEventListener('click', (e) => {
+//         e.preventDefault();
+//         navbarLinks.classList.remove('active');
+//     }, false);
+// }
